33b3fca7fe9e862f283c91f52319d638dfe2a40c
adithya <peddiadithyavardhan42@gmail.com>
2023-02-07 11:49:21 +0530

    added customer home page

--- customer/urls.py
+++ customer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns=[
     path('stock-list/', views.stock_list, name='stocklist'),
-    path('billing-page/<str:pk>/', views.billing, name='billingpage')
+    path('billing-page/<str:pk>/', views.billing, name='billingpage'),
+    path('customer-home/', views.customer_home, name='customer_home')
 ]
