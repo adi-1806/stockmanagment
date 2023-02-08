6e45080b16ce1761dfaff15a488316c00ef01c0e
adithya <peddiadithyavardhan42@gmail.com>
2023-02-08 11:48:46 +0530

    venv created

--- stockmanagment/settings.py
+++ stockmanagment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-on3e%!qpem-9615y#yi@mu%0+rbc+i(@7(kx_0vtxf7_t)8zv+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
