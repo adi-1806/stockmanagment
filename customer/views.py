f232a59684f0df03b0186c76035c8f9aa76ea9b4
adithya <peddiadithyavardhan42@gmail.com>
2023-02-08 15:54:24 +0530

    message added

--- customer/views.py
+++ customer/views.py
@@ -3,6 +3,8 @@ from stock.models import motor_products, other_products
 from .models import selled_motor, selled_other
 from django.contrib.auth.decorators import login_required
 import datetime
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -16,7 +18,7 @@ def stock_list(request):
     present_stock=motor_products.objects.all().order_by("company")
     other_stock=other_products.objects.all().order_by("item_name")
 
-    
+
 
     return render(request, "customer/stock_list.html",{
         "present_stock":present_stock,
@@ -29,35 +31,36 @@ def billing(request,pk):
     if request.method=="POST":
         form=request.POST['Yes']
 
-        if form=='Submit':
-            details.quantity=details.quantity-1
+        if details.quantity>0 :
+            if form=='Submit':
+                details.quantity=details.quantity-1
 
-            entered_name = request.POST['Customer_name']
-            entered_phno = request.POST['Customer_phno']
-            entered_hno = request.POST['H.no']
-            entered_street = request.POST['street']
-            entered_village = request.POST['village']
-            entered_district = request.POST['District']
-            entered_pincode = request.POST['pincode']
-            entered_address = entered_hno +", "+ entered_street +", "+ entered_village +", "+ entered_district +", "+ entered_pincode
- 
-            selled_details=selled_motor(
-                company= details.company,
-                model_name=details.model_name,
-                hp=details.hp,
-                hsncode=details.hsncode,
-                price = details.price,
-                others = details.others,
-                customer_name= entered_name,
-                customer_phno= entered_phno,
-                customer_address = entered_address,
-                dateandtime = datetime.datetime.now()
-            )
-            selled_details.save()
-            details.save()
-        
-
-            return redirect('stocklist')
+                entered_name = request.POST['Customer_name']
+                entered_phno = request.POST['Customer_phno']
+                entered_hno = request.POST['H.no']
+                entered_street = request.POST['street']
+                entered_village = request.POST['village']
+                entered_district = request.POST['District']
+                entered_pincode = request.POST['pincode']
+                entered_address = entered_hno +", "+ entered_street +", "+ entered_village +", "+ entered_district +", "+ entered_pincode
+    
+                selled_details=selled_motor(
+                    company= details.company,
+                    model_name=details.model_name,
+                    hp=details.hp,
+                    hsncode=details.hsncode,
+                    price = details.price,
+                    others = details.others,
+                    customer_name= entered_name,
+                    customer_phno= entered_phno,
+                    customer_address = entered_address,
+                    dateandtime = datetime.datetime.now()
+                )
+                selled_details.save()
+                details.save()
+                return redirect('stocklist')
+            else:
+                messages.info(request, 'Stock not available')
         
 
     return render(request, "customer/billing.html",{
@@ -69,34 +72,37 @@ def billing_others(request,pk):
     if request.method=="POST":
         form=request.POST['Yes']
 
-        if form=='Submit':
-            details.quantity=details.quantity-1
+        if details.quantity>0 :
 
-            entered_name = request.POST['Customer_name']
-            entered_phno = request.POST['Customer_phno']
-            entered_hno = request.POST['H.no']
-            entered_street = request.POST['street']
-            entered_village = request.POST['village']
-            entered_district = request.POST['District']
-            entered_pincode = request.POST['pincode']
-            entered_address = entered_hno +", "+ entered_street +", "+ entered_village +", "+ entered_district +", "+ entered_pincode
- 
-            selled_details=selled_other(
-                item_name= details.item_name,
-                hsncode=details.hsncode,
-                price = details.price,
-                specifications = details.specifications,
-                customer_name= entered_name,
-                customer_phno= entered_phno,
-                customer_address = entered_address,
-                date = datetime.datetime.now()
-            )
-            selled_details.save()
-            details.save()
-        
+            if form=='Submit':
+                details.quantity=details.quantity-1
 
-            return redirect('stocklist')
-        
+                entered_name = request.POST['Customer_name']
+                entered_phno = request.POST['Customer_phno']
+                entered_hno = request.POST['H.no']
+                entered_street = request.POST['street']
+                entered_village = request.POST['village']
+                entered_district = request.POST['District']
+                entered_pincode = request.POST['pincode']
+                entered_address = entered_hno +", "+ entered_street +", "+ entered_village +", "+ entered_district +", "+ entered_pincode
+    
+                selled_details=selled_other(
+                    item_name= details.item_name,
+                    hsncode=details.hsncode,
+                    price = details.price,
+                    specifications = details.specifications,
+                    customer_name= entered_name,
+                    customer_phno= entered_phno,
+                    customer_address = entered_address,
+                    date = datetime.datetime.now()
+                )
+                selled_details.save()
+                details.save()
+            
+
+                return redirect('stocklist')
+        else:
+                messages.info(request, 'Stock not available')
 
     return render(request, "customer/billing_other.html",{
         "details":details
